00b5e544cf68f5652435c3ef2894faf03a07c093
Daehwan Nam <dhnam@postech.ac.kr>
2023-07-13 14:53:24 +0900

    dir-locals update

--- dev-setup.sh
+++ dev-setup.sh
@@ -9,7 +9,8 @@ else
     FILE_PATH=".dir-locals-files/$CONFIG_NAME"
     if [ -f "$FILE_PATH" ]; then
         # ln -s ".dir-locals-files/$CONFIG_NAME" .dir-locals.el  # soft link is not working for .dir-locals.el
-        cp ".dir-locals-files/$CONFIG_NAME" .dir-locals.el
+        ln ".dir-locals-files/$CONFIG_NAME" .dir-locals.el  # hard link
+        # cp ".dir-locals-files/$CONFIG_NAME" .dir-locals.el
         echo ".dir-locals.el is linked to .dir-locals-files/$CONFIG_NAME"
     else
         echo "$FILE_PATH does not exist"
