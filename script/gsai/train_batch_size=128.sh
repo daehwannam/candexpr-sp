bc4bf64ff196277567e975007d85d0a92e8ea76f
Daehwan Nam <dhnam@postech.ac.kr>
2023-08-01 21:50:26 +0900

    config and script update

--- script/gsai/train_batch_size=128.sh
+++ script/gsai/train_batch_size=128.sh
@@ -3,5 +3,5 @@
 source /home/dhnam/program/miniconda3/etc/profile.d/conda.sh
 conda activate kqapro
 
-python -m domain.kqapro.run --config config.train_for_multiple_decoding_strategies \
+python -m domain.kqapro.run --using-tqdm false --config config.train_for_multiple_decoding_strategies \
        --additional-config config.batch.size=128
