552342261ace31d973b8bf865f6a3bf7c23c7466
Daehwan Nam <dhnam@postech.ac.kr>
2024-04-12 16:08:24 +0900

    fix for the option "using_distinctive_union_types" during "train-for-multiple-decoding-strategies"

--- config/kqapro/train_for_multiple_decoding_strategies.py
+++ config/kqapro/train_for_multiple_decoding_strategies.py
@@ -5,12 +5,14 @@ from dhnamlib.pylib.context import Environment
 from dhnamlib.pylib.context import LazyEval
 from dhnamlib.pylib.iteration import distinct_pairs
 from dhnamlib.pylib.structure import AttrDict
+# from dhnamlib.pylib.decoration import deprecated
 
 from domain.kqapro import configuration as kqapro_configuration
 
 from .train_general import config as _config_general
 
 
+# @deprecated
 def _make_grammar_with_no_dut():
     with kqapro_configuration.config.let(using_distinctive_union_types=False):
         return kqapro_configuration._make_grammar()
@@ -37,12 +39,16 @@ _config_specific = Environment(
             constrained_decoding=True,
             using_arg_candidate=False,
             using_distinctive_union_types=False,
+            # grammar_lazy_obj=LazyEval(kqapro_configuration._make_grammar),
+            grammar_lazy_obj=LazyEval(_make_grammar_with_no_dut),
         ),
         AttrDict(
             decoding_strategy_name='no-constrained-decoding',
             constrained_decoding=False,
             using_arg_candidate=False,
             using_distinctive_union_types=False,
+            # grammar_lazy_obj=LazyEval(kqapro_configuration._make_grammar),
+            grammar_lazy_obj=LazyEval(_make_grammar_with_no_dut),
         ),
     ]
 )
